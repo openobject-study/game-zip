8930f60222729579bca3c6f43c2ae57ffaf929ec
이동주 <ikloev32@naver.com>
2024-02-05 16:27:06 +0900

    코드 정리

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,18 +1,6 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import App from 'views/App';
 
-// export default createBrowserRouter(
-//   createRoutesFromElements(
-//     <>
-//       <Route path="/" element={<App />} />
-//       <Route path="/dashboard" element={<div>dashboard</div>} />
-//     </>
-//   )
-// );
 
 export default createBrowserRouter([
   {
